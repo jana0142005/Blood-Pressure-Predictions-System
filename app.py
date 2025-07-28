5980ddf26e663bfc0dce5cb2d31b57715ab1ed9d
jana0142005 <Janapapu014@gmail.com>
2025-07-28 21:01:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -653,7 +653,7 @@ def information_page():
     It is not intended as a substitute for professional medical advice, diagnosis, or treatment. 
     Always seek the advice of your physician or other qualified health provider with any questions 
     you may have regarding a medical condition. Never disregard professional medical advice or 
-    delay in seeking it because of something you have read in this application.
+    delay in seeking it because of something you have read in this application . Created by jana...!
     """)
 
 if __name__ == "__main__":
